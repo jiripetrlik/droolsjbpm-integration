1eb08adf7c4715f501477466eecc65d0fcd652e5
Jiri Petrlik <jiripetrlik@gmail.com>
2018-11-08 09:48:05 +0100

    Reproducer

---
+++ camel-container-tests/camel-container-integration-tests/src/test/java/org/kie/camel/container/integration/tests/RemoteIntegrationTest.java
@@ -0,0 +1,28 @@
+package org.kie.camel.container.integration.tests;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.kie.camel.container.api.model.Person;
+import org.kie.server.api.model.KieContainerResource;
+import org.kie.server.api.model.KieContainerResourceList;
+import org.kie.server.api.model.ReleaseId;
+
+public class RemoteIntegrationTest extends AbstractKieCamelIntegrationTest {
+
+    @Test
+    public void listContainersTest() throws InterruptedException {
+        final String containerId = "test-container";
+        final ReleaseId releaseId = new ReleaseId("org.drools",
+                                            "camel-container-tests-kjar",
+                                            "7.15.0-SNAPSHOT");
+        final KieContainerResource kieContainerResource = new KieContainerResource(releaseId);
+        kieContainerResource.setContainerId(containerId);
+
+        KieContainerResource resource = kieCamelTestService.esCreateContainer(containerId, kieContainerResource);
+        Assertions.assertThat(resource).isNotNull();
+
+        KieContainerResourceList kieContainerResourceList = kieCamelTestService.esListContainers();
+
+        Assertions.assertThat(kieContainerResourceList.getContainers().size()).isGreaterThan(0);
+    }
+}
